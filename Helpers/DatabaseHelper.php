1f069aa951fd9bc8caccb32dc5fe4ec784dac6a3
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-05-02 11:02:19 +0900

    DBにデータ挿入の修正, publcURLから取得

--- Helpers/DatabaseHelper.php
+++ Helpers/DatabaseHelper.php
@@ -10,45 +10,23 @@ use Exception;
 
 class DatabaseHelper
 {
-    public static function createNewSnippetHelper(string $highlight, string $title, string $validTime, string $content, string $hashedVal, bool $publish)
+    public static function createNewImage(string $title, string $shareURL, string $deleteURL, int $size, string $mime, string $imgURL, string $viewedCount, bool $publish, string $ip_address)
     {
         $db = new MySQLWrapper();
-
-        $expiredAt = new DateTime('now', new DateTimeZone('Asia/Tokyo'));
-        $valiable_Time = ValidationHelper::getExpirationTime();
-
-        if ($validTime !== "never") {
-            $expiredAt->add(new DateInterval($valiable_Time[$validTime]));
-            $expiredAtFormatted = $expiredAt->format('Y-m-d H:i:s');
-        } else {
-            $expiredAtFormatted = null;
-        }
-
-        $stmt = $db->prepare("INSERT INTO snippets 
-            (title, url, language, content, expiration, publish,  expire_at)
-            VALUES (?, ?, ?, ?, ?, ?, ? )
+        $stmt = $db->prepare("INSERT INTO images 
+            (title, shared_url, delete_url, size, mimeType, image_url, viewed_count, publish, ip_address)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
             ");
-        $stmt->bind_param('sssssis', $title, $hashedVal, $highlight, $content, $validTime, $publish, $expiredAtFormatted);
+        $stmt->bind_param('sssissiis', $title, $shareURL, $deleteURL, $size, $mime, $imgURL, $viewedCount, $publish, $ip_address);
         $insertSuccess = $stmt->execute();
         $stmt->close();
         return $insertSuccess;
     }
 
-    public static function deleteExpiredAt(string $url): string
+    public static function getImageBySharedURL(string $url): array | string
     {
         $db = new MySQLWrapper();
-        $stmt = $db->prepare("DELETE FROM snippets WHERE url = ?");
-        $stmt->bind_param('s', $url);
-        $stmt->execute();
-        $result = $stmt->execute();
-        if ($result) return "success";
-        return "failed"; 
-    }
-
-    public static function getSnippeter(string $url): array | string
-    {
-        $db = new MySQLWrapper();
-        $stmt = $db->prepare("SELECT * FROM snippets WHERE url = ?");
+        $stmt = $db->prepare("SELECT * FROM images WHERE shared_url = ?");
         $stmt->bind_param('s', $url);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -60,10 +38,22 @@ class DatabaseHelper
         }
     }
 
+    public static function deleteExpiredAt(string $url): string
+    {
+        $db = new MySQLWrapper();
+        $stmt = $db->prepare("DELETE FROM images WHERE url = ?");
+        $stmt->bind_param('s', $url);
+        $stmt->execute();
+        $result = $stmt->execute();
+        if ($result) return "success";
+        return "failed";
+    }
+
+
     public static function getAllSnipetter(): array | string
     {
         $db = new MySQLWrapper();
-        $stmt = $db->prepare("SELECT * FROM snippets WHERE publish = 1 AND (expire_at > NOW() OR expire_at IS NULL)");
+        $stmt = $db->prepare("SELECT * FROM images WHERE publish = 1 AND (expire_at > NOW() OR expire_at IS NULL)");
         $stmt->execute();
         $ans = [];
         $result = $stmt->get_result();
