a5762e9efc976bfe075a100e0cf069a15fc1c498
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-06-10 08:21:26 +0900

    autoload追加

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/..'));
 spl_autoload_extensions(".php");
 spl_autoload_register();
+require '../autoload.php';
+date_default_timezone_set('Asia/Tokyo');
 
 $DEBUG = true;
 
